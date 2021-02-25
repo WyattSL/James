af8c85cffea008402e3b35ac005b75bee9932675
WyattSL <wyattlipscomb10@gmail.com>
2021-02-25 07:53:03 +0100

    Initalize

---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config({path: "./.env"})
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const embed = Discord.MessageEmbed;
+const fs = require("fs");
+
+client.on("ready", () => {
+  console.log(`Client logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.TOKEN)
